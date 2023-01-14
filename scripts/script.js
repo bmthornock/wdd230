5e8e95a76ccf23188f9ed71c850338a2e484dc1e
bmthornock <b.t.thornock@gmail.com>
2023-01-14 11:55:02 -0600

    Lesson2

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+copyright.textContent = `© 2023 .:|:. Brenton M. Thornock .:|:. Somewhere Beyond the Sea`;
+
+const nDateMod = document.querySelector('#copyright');
 
 const date = new Date(document.lastModified);
 document.getElementById("demo").innerHTML = date;
