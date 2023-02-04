3eef739bb68a0470b8fe064de15d0f7b84f3e700
bmthornock <b.t.thornock@gmail.com>
2023-02-04 12:58:54 -0600

    Lesson 5

--- chamber/Scripts/script.js
+++ chamber/Scripts/script.js
@@ -3,8 +3,17 @@ copyright.textContent = '©';
 const date = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = date;
 
-document.querySelector("#dateTime").innerHTML = date;
+
+const currentDate = new Date().toDateString()
+document.querySelector("#dateTime").innerHTML = currentDate;
 
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+const dow = date.getDay();
+
+ if (dow <= 2) {
+    const message = "Come join us at Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    document.querySelector("meetMessage").innerHTML = message;
+ }
